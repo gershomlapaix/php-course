030f7193a5f6fc960732a383ea203e2dff14d757
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-18 22:39:14 +0200

    first commit

---
+++ oop/Person.php
@@ -0,0 +1,12 @@
+<?php
+class Person {
+    public $age;
+    function speak() {
+    echo "Hi!";
+    }
+    }
+
+    $p1 = new Person();
+    $p1->speak();
+
+?>
---
+++ oop/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oop</title>
+</head>
+
+<body>
+    <?php
+    class Student
+    {
+        public $firstName;
+
+        function setFirstName($firstName)
+        {
+            $this->firstName = $firstName;
+
+            echo "This is possible inside :" . $this->firstName . "<br/>";
+        }
+    }
+
+    $std1 = new Student();
+    $std1->setFirstName("La paix");
+
+    $std2 = new Student();
+    $std2->setFirstName(("Gersh"));
+
+    echo "<br/> current time is ".(new DateTime())->format("M-D-Y");
+    PHP_EOL;
+    echo "current time is ".(new DateTime())->format("M-D-Y");
+
+    ?>
+</body>
+
+</html>
