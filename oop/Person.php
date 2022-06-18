d90a7b9d94f5453f569ac77d8588a43c4a960bfb
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-18 23:00:41 +0200

    feat: magic methods, toString

--- oop/Person.php
+++ oop/Person.php
@@ -1,12 +1,40 @@
-<?php
-class Person {
-    public $age;
-    function speak() {
-    echo "Hi!";
-    }
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Person</title>
+</head>
+
+<body>
+    <?php
+    class Person
+    {
+        public $name;
+        public $age;
+
+        public function __construct($name, $age)
+        {
+            $this->name = $name;
+            $this->age = $age;
+        }
+
+        /*
+            how the class will react when it is treated like a string
+        */
+        public function __toString()
+        {
+            $result = $this->name . " is " . $this->age . " years old";
+
+            return $result;
+        }
     }
 
-    $p1 = new Person();
-    $p1->speak();
+    $p = new Person("La paix", 20);
+    echo $p;
+    ?>
+</body>
 
-?>
+</html>
